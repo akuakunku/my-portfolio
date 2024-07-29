2bbe19602e89a4e1ebf78db69c11f5d3f98d93e0
chesko25 <akuakunku869@gmail.com>
2024-07-29 19:42:42 +0700

    up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -69,7 +69,7 @@ const Header = ({ toggleTheme, theme, isAuthenticated, handleLogout }) => {
       </motion.div>
       {menuOpen && (
         <motion.div
-          className={`md:hidden fixed top-3/1 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white p-6 rounded-full w-64 h-64 flex flex-col items-center justify-center space-y-4`}
+          className={`md:hidden fixed top-3/3 left-1/3 transform -translate-x-1/2 -translate-y-1/2 text-white p-4 rounded-full w-64 h-64 flex flex-col items-center justify-center space-y-4`}
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 0 }}
@@ -78,7 +78,7 @@ const Header = ({ toggleTheme, theme, isAuthenticated, handleLogout }) => {
 
           <Link
             to="/"
-            className="flex items-center w-40 py-3 px-3 bg-gray-800 text-white rounded-lg text-sm font-medium justify-center gap-2 hover:bg-gray-700 transition-colors"
+            className="flex items-center w-40 py-2 px-2 bg-gray-800  border border:bg-yellow-500 text-white rounded-lg text-sm font-medium justify-center gap-2 hover:bg-gray-700 transition-colors"
             onClick={handleMenuToggle}
           >
             <FaHome className="text-lg" />
@@ -86,7 +86,7 @@ const Header = ({ toggleTheme, theme, isAuthenticated, handleLogout }) => {
           </Link>
           <Link
             to="/admin-login"
-            className="flex items-center w-40 py-3 px-4 bg-gray-800 text-white rounded-lg text-sm font-medium justify-center gap-2 hover:bg-gray-700 transition-colors"
+            className="flex items-center w-40 py-2 px-2 bg-gray-800  border border:bg-yellow-500 text-white rounded-lg text-sm font-medium justify-center gap-2 hover:bg-gray-700 transition-colors"
             onClick={handleMenuToggle}
           >
             <FaUserShield className="text-lg" />
@@ -94,7 +94,7 @@ const Header = ({ toggleTheme, theme, isAuthenticated, handleLogout }) => {
           </Link>
           <a
             href="https://github.com/chesko21"
-            className="flex items-center w-40 py-3 px-4 bg-gray-800 text-white rounded-lg text-sm font-medium justify-center gap-2 hover:bg-gray-700 transition-colors"
+            className="flex items-center w-40 py-2 px-2 bg-gray-800  border border:bg-yellow-500 text-white rounded-lg text-sm font-medium justify-center gap-2 hover:bg-gray-700 transition-colors"
             >
             <FaGithub className="text-lg" />
             <span>GitHub</span>
@@ -102,7 +102,7 @@ const Header = ({ toggleTheme, theme, isAuthenticated, handleLogout }) => {
             {isAuthenticated && (
               <button
                 onClick={handleLogout}
-                className="flex items-center w-40 py-3 px-4 bg-gray-800 text-white rounded-lg text-sm font-medium justify-center gap-2 hover:bg-red-500 transition-colors"
+              className="flex items-center w-40 py-2 px-2 bg-gray-800  border border:bg-yellow-500 text-white rounded-lg text-sm font-medium justify-center gap-2 hover:bg-red-500 transition-colors"
                 aria-label="Logout"
               >
                 <FaSignOutAlt className="text-lg" />
